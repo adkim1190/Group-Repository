ba3bfad08634c7dbf528c5f0b19c7f93ff072334
Andrew Kim <Andrew Kim>
2016-06-20 14:31:20 -0500

    hi

--- mainbody.sh
+++ mainbody.sh
@@ -3,8 +3,30 @@
 
 
 
+echo "#!/bin/bash
+#MSUB -N ajob
+#MSUB -A b1011
+#MSUB -q ligo
+#MSUB -l walltime=00:00:$2
+#MSUB -l nodes=1:ppn=1
+#MSUB -j oe
+
+cd \$PBS_O_WORKDIR
+
+source $1" > submit;
+
+msub submit;
+rm submit;
 
 
+
+
+log()
+diskspace()
+
+
+
+echo "backup completed in:", 
 
 
 
